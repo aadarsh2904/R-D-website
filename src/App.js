b5c1b6b599dc6f2f16be353272f3eec020c5977d
aadarsh2904 <aadarshranjan2904@gmail.com>
2023-10-19 19:12:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Expensenew from './components/Expensenew.js'
+import Dean from './components/Dean/Dean.js'
+import Research from './components/Research.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+  return (<div>
+    <navbar></navbar>
+    <Dean></Dean>;
+<Expensenew></Expensenew>;
+<Research></Research>;
+<Footer></Footer>
+</div>
   );
 }
 
 export default App;
+
--- src/components/Dean/Dean.js
+++ src/components/Dean/Dean.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Dean.css';
+import styled from 'styled-components';
 import abhi from './abhinandan.jpg'
+const CurvedBox = styled.div`
+  
+background: linear-gradient(to right, #ADD8E6 5%, #FFC0CB 95%);
+
+  background-color: rgba(158, 179, 220, 1);
+  color: white;
+  border-radius: 10px;
+  padding: 5px;
+  text-align: center;
+  margin: auto;
+  outline:none;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-wrap:nowrap;
+  justify-content:space-between;
+  height:90vh;
+`;
 export default function Dean() {
     return (<div className="director">
-    <div className="row">
     <div className="dir-col">
+    <CurvedBox>
       <img src={abhi} alt="Logo" />
       <div className='txt'>
       <h1>Research at IIT INDORE</h1>
@@ -13,7 +31,7 @@ export default function Dean() {
 <p>In this collection, we invite you to embark on a journey of discovery, inspiration, and connection. Our carefully curated photographs capture the essence of who we are, what we do, and the impact we have on our community, industry, and beyond.</p>
     <h3></h3>
       </div>
-    </div>
+      </CurvedBox>
     </div>
     </div>
     );
