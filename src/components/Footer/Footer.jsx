a449e034d7abc761e1aea93c5fee0bf961661a85
aradhya2607 <135439440+aradhya2607@users.noreply.github.com>
2024-03-31 23:33:51 +0530

    links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
             <hr></hr>
             <div className='my__footer-links'>
                 <div className='my__footer-links_div1'>
-                <div style={{ fontSize: '20px' , margin: '8px' , color:'rgb(7, 2, 109)' ,fontWeight: 'bold'}}>CONTACT</div>
+                <div className="headings">CONTACT</div>
 
                     <div className='contact'>
                         <FaHome  className='contact-icon' style={{height:'25px',width:'25px'}}/>
@@ -40,7 +40,7 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className='my__footer-links_div'>
-                <div style={{ fontSize: '20px' , margin: '8px', color:'rgb(7, 2, 109)' ,fontWeight: 'bold'}}>USEFUL LINKS</div>
+                    <div className="headings" >USEFUL LINKS</div>
                     <a href = 'https://rnd.iiti.ac.in/main/rnd_forms'>
                         <p2>R&D Forms</p2>
                     </a>
@@ -59,7 +59,7 @@ export default function Footer() {
                     
                 </div>
                 <div className='my__footer-links_div'>
-                <div style={{ fontSize: '20px' , margin: '8px' , color:'rgb(7, 2, 109)',fontWeight: 'bold' }}>NAVIGATION</div>
+                <div className="headings">NAVIGATION</div>
                     <a href = 'https://www.iiti.ac.in'>
                         <p2>IIT Indore</p2>
                     </a>
@@ -84,10 +84,13 @@ export default function Footer() {
             {/* <iframe className='border-2 rounded-xl w-[18rem] h-[12rem] ml-0' id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=IIT%20Indore%20Simrol+(IIT%20Indore)&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> */}
             <div className='my__footer-below'>
                 <div className='my__footer-below-links'>
-                <FaInstagram className='ig'/>
-                <FaTwitter className='tw'/>
-                <FaFacebook className='fb'/>
-                <FaLinkedin className='tw'/>
+
+                <a href ="#"><FaInstagram className='ig'/></a>
+                <a href=""><FaTwitter className='tw'/></a>
+                <a href=""><FaFacebook className='fb'/></a>
+                <a href="https://www.linkedin.com/in/research-and-development-iit-indore-a37bb3299/">
+                    <FaLinkedin className='tw'/>
+                </a>
 
                 </div>
             </div>
