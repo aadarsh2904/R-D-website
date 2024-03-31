442b1b1a234475b53fe2be49e8f5284865cc43ab
aadarsh2904 <aadarshranjan2904@gmail.com>
2024-03-31 18:27:09 +0530

    Created people page

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,93 +1,91 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper/modules';
-import { People } from '../data/People';
-import { Finance } from '../data/Finance';
+import React from "react";
+import { OFFICE, Finance, People, Purchase } from "./data";
 
-function Card({ image_url, name, position, email, phone }) {
+const TeamPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-gray-200 p-6 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-2 hover:shadow-xl">
-      <div className="w-24 h-24 bg-gradient-to-br from-yellow-400 to-red-500 rounded-full mb-4"></div>
-      <div className="text-lg font-semibold text-gray-800 mb-2">{name}</div>
-      <div className="text-sm text-gray-600 mb-1">{position}</div>
-      <div className="text-sm text-gray-600 mb-1">Email: {email}</div>
-      <div className="text-sm text-gray-600">Phone: {phone}</div>
+    <div>
+      {/* R&D Section */}
+      <div className="flex flex-col justify-center items-center pb-18 mb-16">
+        <h1 className="flex justify-center items-center text-3xl mb-6 mt-12 text-black font-bold" style={{ fontFamily: 'Arial, sans-serif', textShadow: '3px 3px 6px rgba(0, 0, 0, 0.3)' }}>R&D OFFICE</h1>
+        <div className="flex flex-row flex-wrap justify-center items-center">
+          {OFFICE.map((val) => (
+            <div key={val.id} className="scale-95 relative w-[400px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8">
+              <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
+              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
+                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+                <div className="relative flex flex-col items-center justify-center w-full h-full ">
+                  <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
+                  <p className="mb-1 text-lg text-black font-semibold">{val.position}</p>
+                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
+                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* People Section */}
+      <div className="flex flex-row flex-wrap justify-center items-center">
+        {People.map((val) => (
+          <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8">
+            <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
+            <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
+              <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+              <div className="relative flex flex-col items-center justify-center w-full h-full ">
+                <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
+                <p className="mb-1 text-lg text-black font-semibold">{val.position}</p>
+                <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
+                <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Finance Section */}
+      <div className="flex flex-col justify-center items-center mt-20 pt-10 pb-18 mb-20">
+        <h1 className="flex justify-center items-center text-3xl mb-6 text-black font-bold" style={{ fontFamily: 'Arial, sans-serif', textShadow: '3px 3px 6px rgba(0, 0, 0, 0.3)' }}>R&D Finance</h1>
+        <div className="flex flex-row flex-wrap justify-center items-center">
+          {Finance.map((val) => (
+            <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8">
+              <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
+              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
+                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+                <div className="relative flex flex-col items-center justify-center w-full h-full ">
+                  <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
+                  <p className="mb-1 text-lg text-black font-semibold"> {val.position}</p>
+                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
+                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Purchase Section */}
+      <div className="flex flex-col justify-center items-center pb-18 mb-16">
+        <h1 className="flex justify-center items-center text-3xl mb-6 text-black font-bold" style={{ fontFamily: 'Arial, sans-serif', textShadow: '3px 3px 6px rgba(0, 0, 0, 0.3)' }}>R&D Purchase</h1>
+        <div className="flex flex-row flex-wrap justify-center items-center">
+          {Purchase.map((val) => (
+            <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8">
+              <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
+              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
+                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+                <div className="relative flex flex-col items-center justify-center w-full h-full ">
+                  <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
+                  <p className="mb-1 text-lg text-black font-semibold"> {val.position}</p>
+                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
+                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default function Team() {
-  return (
-    <>
-      <h1 className="text-center text-4xl md:text-5xl lg:text-5xl font-bold text-black my-10">
-        R&D Office
-      </h1>
-      <div className="flex flex-wrap justify-center items-center gap-8">
-        <Swiper
-          navigation={true}
-          modules={[Navigation]}
-          className="mySwiper"
-          spaceBetween={30}
-          slidesPerView={3}
-        >
-          {People.map((item, index) => (
-            <SwiperSlide key={item.id}>
-              <Card {...item} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <h1 className="text-center text-4xl md:text-5xl lg:text-5xl font-bold text-black my-10">
-        R&D Finance
-      </h1>
-      <div className="flex flex-wrap justify-center items-center gap-8">
-        <Swiper
-          navigation={true}
-          modules={[Navigation]}
-          className="mySwiper"
-          spaceBetween={30}
-          slidesPerView={3}
-        >
-          {Finance.map((item, index) => (
-            <SwiperSlide key={item.id}>
-              <Card {...item} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <h1 className="text-center text-4xl md:text-5xl lg:text-5xl font-bold text-black my-10">
-        R&D Purchase
-      </h1>
-      <div className="flex flex-wrap justify-center items-center gap-40">
-        <Card
-          name="Mr. Prashant K Sahu"
-          position="Junior Assistant"
-          email="prashantksahu@iiti.ac.in"
-          phone="+91-731-6605552"
-        />
-        <Card
-          name="Mr. Jaiprakash"
-          position="Junior Assistant"
-          email="jp@iiti.ac.in"
-          phone="+91-731-6605552"
-        />
-        </div>
-        <h1 className="text-center text-4xl md:text-5xl lg:text-5xl font-bold text-black my-10">
-          Centre for Translational Research
-        </h1>
-        <div className="flex flex-wrap justify-center items-center gap-8">
-          {/* Add your card for Centre for Translational Research here */}
-          <Card
-            name="Dr. Archana Chaudhary"
-            position="Executive Officer"
-            email="eo-ctr [at] iiti.ac.in"
-            phone="91-731-6603578"
-          />
-        </div>
-
-      
-    </>
-  );
-}
+export default TeamPage;
