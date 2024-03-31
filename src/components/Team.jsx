4085705c9aa413b77a96cfa5cd53e8ae4b4d9521
aadarsh2904 <aadarshranjan2904@gmail.com>
2024-03-31 23:58:27 +0530

    Update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -9,15 +9,15 @@ const TeamPage = () => {
         <h1 className="flex justify-center items-center text-3xl mb-6 mt-12 text-black font-bold" style={{ fontFamily: 'Arial, sans-serif', textShadow: '3px 3px 6px rgba(0, 0, 0, 0.3)' }}>R&D OFFICE</h1>
         <div className="flex flex-row flex-wrap justify-center items-center">
           {OFFICE.map((val) => (
-            <div key={val.id} className="scale-95 relative w-[375px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300">
+            <div key={val.id} className="scale-95 relative w-[375px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300 hover:border-blue-500">
               <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
-              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
-                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-90 bg-opacity-90">
+                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white to-gray-700 rounded-md"></div>
                 <div className="relative flex flex-col items-center justify-center w-full h-full ">
                   <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
-                  <p className="mb-1 text-lg text-black font-semibold">{val.position}</p>
-                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
-                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+                  <p className="mb-1 text-lg font-bold text-black">{val.position}</p>
+                  <p className="mb-1 text-lg text-lightblue font-semibold"><span role="img" aria-label="mail">📧</span> E-mail: <span className="text-lightblue">{val.email}</span></p>
+                  <p className="mb-1 text-lg text-black font-semibold"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
                 </div>
               </div>
             </div>
@@ -28,15 +28,15 @@ const TeamPage = () => {
       {/* People Section */}
       <div className="flex flex-row flex-wrap justify-center items-center">
         {People.map((val) => (
-          <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300">
+          <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300 hover:border-blue-500">
             <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
-            <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
-              <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+            <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-90 bg-opacity-90">
+              <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white to-gray-700 rounded-md"></div>
               <div className="relative flex flex-col items-center justify-center w-full h-full ">
                 <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
-                <p className="mb-1 text-lg text-black font-semibold">{val.position}</p>
-                <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
-                <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+                <p className="mb-1 text-lg font-bold text-black">{val.position}</p>
+                <p className="mb-1 text-lg text-lightblue font-semibold"><span role="img" aria-label="mail">📧</span> E-mail: <span className="text-lightblue">{val.email}</span></p>
+                <p className="mb-1 text-lg text-black font-semibold"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
               </div>
             </div>
           </div>
@@ -48,15 +48,15 @@ const TeamPage = () => {
         <h1 className="flex justify-center items-center text-3xl mb-6 text-black font-bold" style={{ fontFamily: 'Arial, sans-serif', textShadow: '3px 3px 6px rgba(0, 0, 0, 0.3)' }}>R&D Finance</h1>
         <div className="flex flex-row flex-wrap justify-center items-center">
           {Finance.map((val) => (
-            <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300">
+            <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300 hover:border-blue-500">
               <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
-              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
-                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-90 bg-opacity-90">
+                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white to-gray-700 rounded-md"></div>
                 <div className="relative flex flex-col items-center justify-center w-full h-full ">
                   <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
-                  <p className="mb-1 text-lg text-black font-semibold"> {val.position}</p>
-                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
-                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+                  <p className="mb-1 text-lg font-bold text-black">{val.position}</p>
+                  <p className="mb-1 text-lg text-lightblue font-semibold"><span role="img" aria-label="mail">📧</span> E-mail: <span className="text-lightblue">{val.email}</span></p>
+                  <p className="mb-1 text-lg text-black font-semibold"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
                 </div>
               </div>
             </div>
@@ -69,15 +69,15 @@ const TeamPage = () => {
         <h1 className="flex justify-center items-center text-3xl mb-6 text-black font-bold" style={{ fontFamily: 'Arial, sans-serif', textShadow: '3px 3px 6px rgba(0, 0, 0, 0.3)' }}>R&D Purchase</h1>
         <div className="flex flex-row flex-wrap justify-center items-center">
           {Purchase.map((val) => (
-            <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300">
+            <div key={val.id} className="scale-95 relative w-[300px] overflow-hidden rounded-3xl shadow-lg group bg-white m-8 border border-gray-300 hover:border-blue-500">
               <img className="object-cover w-full h-72 xl:h-80" src={val.image_url} alt="Team Member Name" />
-              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-100">
-                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white opacity-60 to-gray-700 rounded-md"></div>
+              <div className="absolute inset-0 px-6 py-4 text-center duration-300 opacity-0 group-hover:opacity-90 bg-opacity-90">
+                <div className="absolute inset-0 w-full h-full bg-gradient-to-br from-white to-gray-700 rounded-md"></div>
                 <div className="relative flex flex-col items-center justify-center w-full h-full ">
                   <p className="mb-1 text-xl font-bold text-black">{val.name}</p>
-                  <p className="mb-1 text-lg text-black font-semibold"> {val.position}</p>
-                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="mail">📧</span> E-mail: {val.email}</p>
-                  <p className="mb-1 text-lg text-black"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
+                  <p className="mb-1 text-lg font-bold text-black">{val.position}</p>
+                  <p className="mb-1 text-lg text-lightblue font-semibold"><span role="img" aria-label="mail">📧</span> E-mail: <span className="text-lightblue">{val.email}</span></p>
+                  <p className="mb-1 text-lg text-black font-semibold"><span role="img" aria-label="phone">📞</span> Phone: {val.phone}</p>
                 </div>
               </div>
             </div>
